89c033ec66b7fd631816b4a519fbccbd2b99e889
Denny Kluge <denny.kluge@gmail.com>
2019-05-20 23:04:26 +0200

    added a text view

--- app/src/main/java/de/diskostu/android/aboutme/MainActivity.kt
+++ app/src/main/java/de/diskostu/android/aboutme/MainActivity.kt
@@ -1,7 +1,7 @@
 package de.diskostu.android.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 

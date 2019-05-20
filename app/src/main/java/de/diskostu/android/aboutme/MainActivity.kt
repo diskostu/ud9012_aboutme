f501b5de766d27575f556ccb3f0615287a4768c0
Denny Kluge <denny.kluge@gmail.com>
2019-05-21 01:01:36 +0200

    added a click listener to the button

--- app/src/main/java/de/diskostu/android/aboutme/MainActivity.kt
+++ app/src/main/java/de/diskostu/android/aboutme/MainActivity.kt
@@ -1,12 +1,31 @@
 package de.diskostu.android.aboutme
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        done_button.setOnClickListener { view ->
+            addNickname(view)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        nickname_text.text = nickname_edit.text
+
+        nickname_edit.visibility = View.GONE
+        view.visibility = View.GONE
+        nickname_text.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }

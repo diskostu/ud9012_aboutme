e3b23a69d4dbbfabb32da9d87332f933dc40de05
Denny Kluge <denny.kluge@gmail.com>
2019-05-21 22:05:24 +0200

    added databinding (hell. this is ugly.)

--- app/src/main/java/de/diskostu/android/aboutme/MainActivity.kt
+++ app/src/main/java/de/diskostu/android/aboutme/MainActivity.kt
@@ -5,25 +5,42 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import de.diskostu.android.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        done_button.setOnClickListener { view ->
+        // variant 1: use of data binding
+        binding.doneButton.setOnClickListener { view ->
             addNickname(view)
         }
+
+        // variant 2: use of kotlin extensions
+//        done_button.setOnClickListener { view ->
+//            addNickname(view)
+//        }
     }
 
+    /**
+     * We use data binding in here.
+     */
     private fun addNickname(view: View) {
-        nickname_text.text = nickname_edit.text
+        binding.apply {
+            nicknameText.text = binding.nicknameEdit.text
 
-        nickname_edit.visibility = View.GONE
-        view.visibility = View.GONE
-        nickname_text.visibility = View.VISIBLE
+            invalidateAll()
+
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
